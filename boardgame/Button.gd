45e5b162f2e870bdee716ef95c870afee3bfa5f3
vipe4ka <vipe4ka>
2023-11-06 19:42:31 +0300

    add menu

--- boardgame/Button.gd
+++ boardgame/Button.gd
@@ -1,17 +1,10 @@
-extends Node
+extends Button
 
-
-var player
-var day
-var score_user
-var score_hacker
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = "user"
-	day = 1
-	score_user = 0
-	score_hacker = 0
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
