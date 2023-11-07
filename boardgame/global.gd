cde012ec388b0b6e18ff5b8dda8e671d3dadb939
zlata <zlata>
2023-11-07 13:54:54 +0300

    add tasks

--- boardgame/global.gd
+++ boardgame/global.gd
@@ -6,31 +6,31 @@ var hacker
 var day
 var score_user
 var score_hacker
-var count_tasks = {1: 3, 2: 4, 3: 5}
+var count_tasks = {1: 4, 2: 5, 3: 6}
 var user_tasks = [
-	"Скачать браузер Амиго в Microsoft Internet Explorer - браузер",
-	"Скачать и создать почту Mail.ru – майл ру агент",
-	"Занести данные клиента в таблицу Excel - эксель",
-	"Проверить электронную почту – майл ру",
-	"Написать отчет и отправить начальнику - ворд",
-	"Поработать в 1С – 1С.Бухгалтерия",
-	"Поздравить коллегу с ДР в ОК - браузер",
-	"Проверить Telegram - телега",
-	"Нарисовать открытку маме в Paint - пэйнт",
-	"Обработать фото кошки в гимпе - гимп",
-	"Посмотреть анкеты в «Дай винчике» - телега",
-	"Оплатить лицензию WinRar - винрар",
-	"Обновить корзину до новой версии - корзина",
-	"Проверить пк на вирусы в Avast - антивирус",
-	"Позвонить маме в скайп - скайп",
-	"Очистить корзину - корзина"
+	"Скачать браузер Амиго в Microsoft Internet Explorer",
+	"Скачать и создать почту Mail.ru",
+	"Занести данные клиента в таблицу Excel",
+	"Проверить электронную почту",
+	"Написать отчет и отправить начальнику",
+	"Поработать в 1С.Бухгалтерия",
+	"Поздравить коллегу с ДР в ОК",
+	"Проверить Telegram",
+	"Нарисовать открытку маме в Paint",
+	"Обработать фото кошки в гимпе",
+	"Посмотреть анкеты в «Дай винчике»",
+	"Оплатить лицензию WinRar",
+	"Обновить корзину до новой версии",
+	"Проверить пк на вирусы в Avast",
+	"Позвонить маме в скайп",
+	"Очистить корзину"
 ]
 var hacker_tasks = [
-	"Внедрение вредоносных скриптов в незащищенные сайты ",
+	"Внедрение вредоносных скриптов в сайты",
 	"Распространение вредоносного по",
 	"Украсть корпоративные данные из таблиц",
 	"Отправка фишинговых писем через почту",
-	"Закинуть файл ворд с вредоносным макросом.",
+	"Закинуть файл с вредоносным макросом.",
 	"Бомбер сотрудников.",
 	"Поставить «единички» коллегам",
 	"Взломать аккаунт в Телеграм",
@@ -48,14 +48,15 @@ var completedTasks_hacker = 0
 var card_pressed = false
 var index_current_card = 0
 var items_user = [
-	"res://sprites/sprite/iconetxt.png",
-	"res://sprites/sprite/iconetxt.png",
-	"res://sprites/sprite/iconetxt.png"
+	"res://sprites/iconshack/ddos.png",
+	"res://sprites/iconklerk/firewall.png",
+	"res://sprites/iconklerk/strong pass.png"
 ]
 var items_hacker = [
-	"res://sprites/sprite/iconetxt.png",
-	"res://sprites/sprite/iconetxt.png",
-	"res://sprites/sprite/iconetxt.png"
+	"res://sprites/iconshack/ddos.png",
+	"res://sprites/iconshack/fishing.png",
+	"res://sprites/iconshack/sql ink.png",
+	"res://sprites/iconshack/troyan.png"
 ]
 var items_user_inventory = [null, null, null, null , null, null]
 var items_hacker_inventory = [null, null, null, null , null, null]
