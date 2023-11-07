effdc550d326b3a0903d0989782bd6811ffbcf77
vipe4ka <vipe4ka>
2023-11-07 09:04:07 +0300

    add map

--- boardgame/background.gd
+++ boardgame/background.gd
@@ -3,16 +3,19 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var config = get_node("/root/Global")
-	for i in range(config.count_tasks[config.day]):
-		var task = PanelContainer.new()
-		var icon = load("res://sprites/sprite/iconetxt.png")
-		task
-		#task.draw_texture(icon, Vector2(100, 10*i), Color(0,0,0))
-		var window = get_parent().get_node("inventoryFrame/window")
-		window.add_child(task)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func print_map():
+	var posX = -180
+	var posY = -280
+	for i in 6:
+		var row = []
+		for j in 9:
+			var icon = Sprite2D.new()
+			
--- boardgame/toolbar.gd
+++ boardgame/toolbar.gd
@@ -2,13 +2,9 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.visible = false
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_button_pressed():
-	self.visible = not self.visible
